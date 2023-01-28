81f0d46ac1a3154bb8347663cfa0db1ad36ff232
abhishek0151 <106574297+abhishek0151@users.noreply.github.com>
2023-01-28 19:49:12 +0530

    Delete Two file directory

--- Two file/data.js
+++
@@ -1,6 +0,0 @@
-const data=[
-  {name:'anil',email:'anil@testcom',city:'indore'},
-  {name:'anil',email:'anil@testcom',city:'indore'},
-  {name:'anil',email:'anil@testcom',city:'indore'}
-  ];
-module.exports=data;
--- Two file/index.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.createServer(()=>{
-  resp.writeHead(200,{'Content-Type':'application\json'});
-  resp.write(JSON.Stringify(data));
-  resp.end()
-}).listen(5000);
